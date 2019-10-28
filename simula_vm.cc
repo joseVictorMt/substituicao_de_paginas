167a09a24289d1fe10a1ea7945951e911bdbbfc6
José Victor <jose.victor691@gmail.com>
2019-10-28 13:29:05 -0300

    + Montando a estrutura da classe Simulador

--- simula_vm.cc
+++ simula_vm.cc
@@ -3,21 +3,52 @@
 #include <sstream>
 #include <string>
 #include <utility>
+#include <algorithm>
 #include <vector>
 
 using std::cerr, std::cout, std::endl, std::vector, std::pair;
 
-int qtdQuadros( char* );
-pair<int, vector<int>> cadeiaDeReferencias( char* );
+int qtdQuadros( const char* );
+pair<int, vector<int>> cadeiaDeReferencias( const char* );
 
 class Simulador {
     public:
-        Simulador(int qtd_quadros, pair<int,vector<int>> params) : 
-            qtd_quadros(qtd_quadros), qtd_refs(params.first), refs(params.second) {}
+        Simulador( const int qtd_quadros, const int qtd_refs, const vector<int> refs ) : 
+        qtd_quadros(qtd_quadros), qtd_refs(qtd_refs), refs(refs)
+        {
+            memoria = vector<int>(qtd_quadros, -1);
+        }
+
+        int getQtdQuadros() const {
+            return qtd_quadros;
+        }
+
+        int getQtdRefs() const {
+            return qtd_refs;
+        }
+
+        int fifo() {
+            limparMemoria();
+            return 0;
+        }
+
+        int opt() {
+            limparMemoria();
+            return 0;
+        }
+
+        int lru() {
+            limparMemoria();
+            return 0;
+        }
 
     private:
         int qtd_quadros, qtd_refs;
-        vector<int> refs;
+        vector<int> refs, memoria;
+
+        void limparMemoria() {
+            std::for_each( memoria.begin(), memoria.end(), []( auto x ){ x = -1; } );
+        }
 };
 
 int main( int argc, char* argv[] ) {
@@ -28,12 +59,14 @@ int main( int argc, char* argv[] ) {
         exit(-1);
     }
 
-    auto resultado = Simulador( qtdQuadros(argv[1]), cadeiaDeReferencias(argv[2]) );
+    pair<int, vector<int>> params = cadeiaDeReferencias( argv[2] );
+
+    auto resultado = Simulador( qtdQuadros(argv[1]), params.first, params.second );
 
     return 0;
 }
 
-int qtdQuadros( char* qtd_quadros ) {
+int qtdQuadros( const char* qtd_quadros ) {
     int quadros;
     std::istringstream ss (qtd_quadros);
 
@@ -46,7 +79,7 @@ int qtdQuadros( char* qtd_quadros ) {
     return quadros;
 }
 
-pair<int, vector<int>> cadeiaDeReferencias( char* nome_arquivo ) {
+pair<int, vector<int>> cadeiaDeReferencias( const char* nome_arquivo ) {
 
     std::ifstream arquivo (nome_arquivo);
     if( !arquivo.is_open() ) {
@@ -57,14 +90,12 @@ pair<int, vector<int>> cadeiaDeReferencias( char* nome_arquivo ) {
 
     vector<int> refs;
     int ref, qtd_refs = 0;
-    
     while( arquivo >> ref ) {
         refs.push_back( ref );
         ++qtd_refs;
     }
 
     refs.resize(qtd_refs);
-
     arquivo.close();
 
     return pair<int, vector<int>> (qtd_refs, refs);
